32d07a4be044ee81482cf2c387c11ad8a10f5118
CrhystelV <149093684+CrhystelV@users.noreply.github.com>
2024-02-17 23:52:39 -0500

    muy dificil pa mi cerebro de infante, mala persona, hasta con github tengo problema T_T

---
+++ InfoPersonas.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InfoPersonas {
+    public String nombres ;
+    public String cargos ;
+
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(String cargos) {
+        this.cargos = cargos;
+    }
+
+    public InfoPersonas() {
+
+    }
+    public void agregarPersona(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nombre responsable: ");
+        nombres = scanner.nextLine();
+        System.out.println("Cargo: ");
+        cargos= scanner.nextLine();
+
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String [] args){
+        Scanner opcion = new Scanner(System.in);
+        int opc = 0;
+        System.out.println("Ingrese una opción:\n" +
+                "1.Crear proyecto\n" +
+                "2.Ingresar tarea\n" +
+                "3.Estado tareas\n" +
+                "4. Cambiar estado de tareas");
+        opc= Integer.parseInt(opcion.nextLine());
+        switch(opc){
+            case 1:
+                Archivos archivo = new Archivos();
+                archivo.creaArchivo();
+
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+
+                break;
+        }
+
+    }
+}
